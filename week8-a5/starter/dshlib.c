2654e060a531ee60c8e9ca7dc393c0dddef9fd5d
poko102 <aliciaaitseddik@gmail.com>
2025-03-01 22:49:21 -0500

    code with tests and questions doen

--- week8-a5/starter/dshlib.c
+++ week8-a5/starter/dshlib.c
@@ -53,7 +53,7 @@
  *      fork(), execvp(), exit(), chdir()
  */
 
- extern void print_dragon(void);
+ //extern void print_dragon(void);
  #ifndef CMD_ERR_EXECUTE
  #define CMD_ERR_EXECUTE "error: execution failed"
  #endif
@@ -108,6 +108,185 @@
      return OK;
  }
  
+//tokenization for both pipe splitting and double-quoted tokens
+ int build_cmd_list(char *cmd_line, command_list_t *clist)
+{
+    //duplicates full input line to modify
+    char *line_dup = strdup(cmd_line);
+    if (!line_dup)
+    {
+        return ERR_MEMORY;  
+    }
+
+    //command count and index for current command from list
+    clist->num = 0;
+    int current_cmd = 0;
+    
+    //sets starting point of 1st command to beginning of line_dup
+    clist->commands[current_cmd]._cmd_buffer = line_dup;
+    clist->commands[current_cmd].argc = 0;
+    
+    //tracks if we are inside qoutes and pointer that points to start of a token
+    bool in_quote = false;  
+    char *token_start = NULL;
+    
+    //iterates over each character in dupe input and if if quote detected, switch flag  
+    char *p = line_dup;
+    while (*p != '\0') 
+    {
+        if (*p == '"') 
+        {
+            if (!in_quote) 
+            {
+                in_quote = true;
+                //skips opening quote and starts token
+                token_start = p + 1;
+            } 
+            else 
+            {
+                in_quote = false;
+                //finds closing quote and ends the token while skipping closing qoute
+                *p = '\0';
+
+                //if token is not empty, add it to current cmd list
+                if (token_start && strlen(token_start) > 0 && clist->commands[current_cmd].argc < CMD_ARGV_MAX - 1)
+                {
+                    clist->commands[current_cmd].argv[clist->commands[current_cmd].argc++] = token_start;
+                }
+                token_start = NULL; 
+            }
+        }
+        //if there's a pipe and not inside a quotes
+        else if (!in_quote && *p == PIPE_CHAR) 
+        {
+            //if a token is in progress,  add it and then terminate
+            if (token_start) 
+            {
+                *p = '\0';
+                if (strlen(token_start) > 0 && clist->commands[current_cmd].argc < CMD_ARGV_MAX - 1)
+                {
+                    clist->commands[current_cmd].argv[clist->commands[current_cmd].argc++] = token_start;
+                }
+                token_start = NULL;
+            }
+
+            //terminates argv array for current cmd and moves on to next one
+            clist->commands[current_cmd].argv[clist->commands[current_cmd].argc] = NULL;
+            current_cmd++;
+
+            //break if too many cmds 
+            if (current_cmd >= CMD_MAX)
+            {
+                break; 
+            }
+
+            //sets next cmd's starting point to character after pipe
+            clist->commands[current_cmd]._cmd_buffer = p + 1;
+            clist->commands[current_cmd].argc = 0;
+        }
+
+        //if there's whitespace not in qoutes
+        else if (!in_quote && isspace((unsigned char)*p)) 
+        {
+            if (token_start) 
+            {
+                *p = '\0';
+                if (strlen(token_start) > 0 && clist->commands[current_cmd].argc < CMD_ARGV_MAX - 1)
+                {
+                    clist->commands[current_cmd].argv[clist->commands[current_cmd].argc++] = token_start;
+                }
+                token_start = NULL;
+            }
+        }
+        //for any other characters
+        else 
+        {
+            //if token_start not set, mark as beginning of a new token and move to next character
+            if (token_start == NULL && !isspace((unsigned char)*p))
+            {
+                token_start = p;
+            }
+        }
+        p++; 
+    }
+    //if a token is still in progress, add it 
+    if (token_start && strlen(token_start) > 0 && clist->commands[current_cmd].argc < CMD_ARGV_MAX - 1)
+    {
+        clist->commands[current_cmd].argv[clist->commands[current_cmd].argc++] = token_start;
+    }
+
+    //terminate argv array for last cmd and set total num of cmds in list
+    clist->commands[current_cmd].argv[clist->commands[current_cmd].argc] = NULL;
+    clist->num = current_cmd + 1;
+    return OK;
+}
+
+
+
+// executes commands connected by pipes
+int execute_pipeline(command_list_t *clist)
+{
+    //num of cmds
+    int n = clist->num; 
+    //for n cmd, n-1 pipes are needed
+    int pipefds[2 * (n - 1)];
+    //creates a pipe and stores file descriptors
+    for (int i = 0; i < n - 1; i++) 
+    {
+        if (pipe(pipefds + i*2) < 0) 
+        {
+            perror("pipe");
+            return ERR_EXEC_CMD;
+        }
+    }
+    
+    //store child process IDS
+    pid_t pids[n];
+
+    for (int i = 0; i < n; i++) 
+    {
+        //forks a child process for each cmd
+        pids[i] = fork();
+        if (pids[i] < 0) 
+        {
+            perror("fork");
+            return ERR_EXEC_CMD;
+        }
+
+        else if (pids[i] == 0) 
+        { 
+            //if not first cmd, redirect STDIN from previous pipe
+            if (i != 0) {
+                dup2(pipefds[(i - 1) * 2], STDIN_FILENO);
+            }
+            //if not last cmd, redirect STDOUT to current pipe's write 
+            if (i != n - 1) {
+                dup2(pipefds[i * 2 + 1], STDOUT_FILENO);
+            }
+            //close pipe file descriptors in child process
+            for (int j = 0; j < 2 * (n - 1); j++) {
+                close(pipefds[j]);
+            }
+            //cxecute cmd using execvp.
+            execvp(clist->commands[i].argv[0], clist->commands[i].argv);
+            perror(CMD_ERR_EXECUTE);
+            exit(1);
+        }
+    }
+    //closes pipe file descriptors for parent
+    for (int i = 0; i < 2 * (n - 1); i++) 
+    {
+        close(pipefds[i]);
+    }
+
+    //waits for children to finish
+    int status;
+    for (int i = 0; i < n; i++) 
+    {
+        waitpid(pids[i], &status, 0);
+    }
+    return OK;
+}
  
  int exec_local_cmd_loop()
  {
@@ -115,7 +294,7 @@
      int rc = 0;
      cmd_buff_t cmd;
  
-     // memory allocation for the command buffer
+     //memory allocation for the command buffer
      cmd_buff = malloc(SH_CMD_MAX);
      if (cmd_buff == NULL) 
      {
@@ -160,173 +339,200 @@
  
  
          // TODO IMPLEMENT parsing input to cmd_buff_t *cmd_buff
- 
-         //uses build_cmd_buff to duplicate input
-         rc = build_cmd_buff(cmd_buff, &cmd);
-         if (rc != OK) 
+
+         //if input has pipe char then process as pipeline
+         if (strchr(cmd_buff, PIPE_CHAR) != NULL)
          {
-             if (rc == WARN_NO_CMDS)
-                 printf("%s\n", CMD_WARN_NO_CMD); 
-             else
-                 fprintf(stderr, "Error: command parsing failed\n");
-             continue;
-         }
-        
-         
-         //trims leading whitespace
-         char *start = cmd._cmd_buffer;
-         while (*start && isspace((unsigned char)*start))
-         {
-             start++;
-         }
-         if (start != cmd._cmd_buffer)
-         {
-             memmove(cmd._cmd_buffer, start, strlen(start) + 1);
-         }
-         //trims trailing whitespace
-         size_t len = strlen(cmd._cmd_buffer);
-         while (len > 0 && isspace((unsigned char)cmd._cmd_buffer[len - 1])) 
-         {
-             cmd._cmd_buffer[len - 1] = '\0';
-             len--;
-         }
-        
-         //clear and continue if trimmed command is empty
-         if (strlen(cmd._cmd_buffer) == 0) 
-         {
-             clear_cmd_buff(&cmd);
-             continue;
-         }
-        
-         //splits command string into tokens and preserves text in double quotes
-         char *p = cmd._cmd_buffer;
-         cmd.argc = 0;
-         while (*p) 
-         {
-             //skips extra whitespace
-             while (*p && isspace((unsigned char)*p))
-             {
-                 p++;
-             }
-             if (*p == '\0')
-             {
-                 break;
-             }
-             char *token = NULL;
-             
-             if (*p == '"') 
-             {
-                 //skips opening quote and starts token
-                 p++;             
-                 token = p;
-                 while (*p && *p != '"')
-                 {
-                 //finds closing quote and ends the token while skipping closing qoute
-                     p++;  
-                 }     
-                 if (*p == '"') 
-                 {
-                     *p = '\0';
-                     p++;       
-                 }
-             } 
-             else 
-             {
-                 ///marks start of token and moves, if there is whitespace, terminate token
-                 token = p; 
-                 while (*p && !isspace((unsigned char)*p))
-                     p++;   
-                 if (*p != '\0') 
-                 {
-                     *p = '\0';
-                     p++;       
-                 }
-             }
-             //adds token to argv array
-             if (cmd.argc < CMD_ARGV_MAX - 1)
-             {
-                 cmd.argv[cmd.argc++] = token;
-             }
-             else 
-             {
-                 return ERR_TOO_MANY_COMMANDS;
-                 break;
-             }
-         }
-         cmd.argv[cmd.argc] = NULL;
- 
-         if (cmd.argc == 0 || cmd.argv[0] == NULL) {
-             clear_cmd_buff(&cmd);
-             continue;
-         }
-         
-        
-         //checks if command is "cd" and if so, change directory
-         if (strcmp(cmd.argv[0], "cd") == 0) 
-         {
-             if (cmd.argc < 2) {
-                 clear_cmd_buff(&cmd);
-                 continue;
-             }
-             if (chdir(cmd.argv[1]) != 0) 
-             {
-                 perror("chdir failed");
-             }
-             clear_cmd_buff(&cmd);
-             continue;
-         }
-        
-         //if command is "rc", output return code of last external command
-         if (strcmp(cmd.argv[0], "rc") == 0) 
-         {
-             printf("%d\n", rc);
-             clear_cmd_buff(&cmd);
-             continue;
-         }
-        
-         // TODO IMPLEMENT if built-in command, execute builtin logic for exit, cd (extra credit: dragon)
-         if (strcmp(cmd.argv[0], "dragon") == 0) 
-         {
-             print_dragon();
-             clear_cmd_buff(&cmd);
-             continue;
-         }
- 
-         
-        
-     
-         // TODO IMPLEMENT if not built-in command, fork/exec as an external command
-         // for example, if the user input is "ls -l", you would fork/exec the command "ls" with the arg "-l"
- 
- 
-         //forks a child process to execute external command
-         pid_t pid = fork();
-         if (pid < 0) 
-         {
-             perror("fork failed");
-             clear_cmd_buff(&cmd);
-             continue;
-         } 
-         else if (pid == 0) 
-         {
-             //in child process, execute command using execvp and if it returns, give an error
-             execvp(cmd.argv[0], cmd.argv);
-             perror(CMD_ERR_EXECUTE);
-             exit(1);
-         } 
-         else 
-         {
-             //parent process waits for child process to be done
-             int status;
-             waitpid(pid, &status, 0);
-             if (WIFEXITED(status))
-                 rc = WEXITSTATUS(status);
-         }
-        
-         clear_cmd_buff(&cmd);
-     }
+
+            command_list_t clist;
+            //builds list of cmds split by uqouted pipes
+            rc = build_cmd_list(cmd_buff, &clist);
+            if (rc != OK) 
+            {
+                if (rc == WARN_NO_CMDS)
+                    printf("%s\n", CMD_WARN_NO_CMD); 
+                else
+                    fprintf(stderr, "Error: command parsing failed\n");
+                continue;
+            }
+            
+            //excute pipline
+            rc = execute_pipeline(&clist);
+            if (rc != OK)
+            {
+                fprintf(stderr, "Pipeline execution failed\n");
+            }
+            continue;
+        }
+        else
+        {
+            //if no pipe, process as a single cmd
+            rc = build_cmd_buff(cmd_buff, &cmd);
+            if (rc != OK) 
+            {
+                if (rc == WARN_NO_CMDS)
+                    printf("%s", CMD_WARN_NO_CMD);
+                else
+                    fprintf(stderr, "Error: command parsing failed\n");
+                continue;
+            }
+
+            //trims leading whitespace
+            char *start = cmd._cmd_buffer;
+            while (*start && isspace((unsigned char)*start))
+            {
+                start++;
+            }
+            if (start != cmd._cmd_buffer)
+            {
+                memmove(cmd._cmd_buffer, start, strlen(start) + 1);
+            }
+
+
+            //trims trailing whitespace
+            size_t len = strlen(cmd._cmd_buffer);
+            while (len > 0 && isspace((unsigned char)cmd._cmd_buffer[len - 1])) 
+            {
+                cmd._cmd_buffer[len - 1] = '\0';
+                len--;
+            }
+            
+            //clear and continue if trimmed command is empty
+            if (strlen(cmd._cmd_buffer) == 0) 
+            {
+                clear_cmd_buff(&cmd);
+                continue;
+            }
+            
+            //splits command string into tokens and preserves text in double quotes
+            char *p = cmd._cmd_buffer;
+            cmd.argc = 0;
+            while (*p) 
+            {
+                //skips extra whitespace
+                while (*p && isspace((unsigned char)*p))
+                {
+                    p++;
+                }
+                if (*p == '\0')
+                {
+                    break;
+                }
+                char *token = NULL;
+                
+                if (*p == '"') 
+                {
+                    //skips opening quote and starts token
+                    p++;             
+                    token = p;
+                    while (*p && *p != '"')
+                    {
+                    //finds closing quote and ends the token while skipping closing qoute
+                        p++;  
+                    }     
+                    if (*p == '"') 
+                    {
+                        *p = '\0';
+                        p++;       
+                    }
+                } 
+                else 
+                {
+                    ///marks start of token and moves, if there is whitespace, terminate token
+                    token = p; 
+                    while (*p && !isspace((unsigned char)*p))
+                        p++;   
+                    if (*p != '\0') 
+                    {
+                        *p = '\0';
+                        p++;       
+                    }
+                }
+                //adds token to argv array
+                if (cmd.argc < CMD_ARGV_MAX - 1)
+                {
+                    cmd.argv[cmd.argc++] = token;
+                }
+                else 
+                {
+                    return ERR_TOO_MANY_COMMANDS;
+                    break;
+                }
+            }
+            cmd.argv[cmd.argc] = NULL;
     
-     //frees buff
-     free(cmd_buff);
-     return OK;
- }
+            /*
+            if (cmd.argc == 0 || cmd.argv[0] == NULL)
+            {
+                clear_cmd_buff(&cmd);
+                continue;
+            }
+            */
+            
+            //checks if command is "cd" and if so, change directory 
+            if (strcmp(cmd.argv[0], "cd") == 0) 
+            {
+                if (cmd.argc < 2) 
+                {
+                    clear_cmd_buff(&cmd);
+                    continue;
+                }
+                if (chdir(cmd.argv[1]) != 0) 
+                {
+                    perror("chdir failed");
+                }
+                clear_cmd_buff(&cmd);
+                continue;
+            }
+            
+            //if command is "rc", output return code of last external command
+            if (strcmp(cmd.argv[0], "rc") == 0) 
+            {
+                printf("%d\n", rc);
+                clear_cmd_buff(&cmd);
+                continue;
+            }
+            
+            /*
+            // TODO IMPLEMENT if built-in command, execute builtin logic for exit, cd (extra credit: dragon)
+            if (strcmp(cmd.argv[0], "dragon") == 0) 
+            {
+                print_dragon();
+                clear_cmd_buff(&cmd);
+                continue;
+            }
+            */
+            
+            // TODO IMPLEMENT if not built-in command, fork/exec as an external command
+            // for example, if the user input is "ls -l", you would fork/exec the command "ls" with the arg "-l"
+ 
+            //forks a child process to execute external command
+            pid_t pid = fork();
+            if (pid < 0) 
+            {
+                perror("fork failed");
+                clear_cmd_buff(&cmd);
+                continue;
+            } 
+            else if (pid == 0) 
+            {
+                //in child process, execute command using execvp and if it returns, give an error
+                execvp(cmd.argv[0], cmd.argv);
+                perror(CMD_ERR_EXECUTE);
+                exit(1);
+            } 
+            else 
+            {
+                //parent process waits for child process to be done
+                int status;
+                waitpid(pid, &status, 0);
+                if (WIFEXITED(status))
+                    rc = WEXITSTATUS(status);
+            }   
+            clear_cmd_buff(&cmd);
+        }
+     }
+        //frees buff
+        free(cmd_buff);
+        return rc;
+    }
--- week8-a5/starter/questions.md
+++ week8-a5/starter/questions.md
@@ -1,15 +1,15 @@
 1. Your shell forks multiple child processes when executing piped commands. How does your implementation ensure that all child processes complete before the shell continues accepting user input? What would happen if you forgot to call waitpid() on all child processes?
 
-_answer here_
+My shell calls waitpid() for every child process which makes sure that every command is complete before the shell goes back to prompting the user for new input. If I forgot to call it on the children processes, the parent would not collect the finished children's exit status which would lead to the system keeping the record and wasting space/resources.
 
 2. The dup2() function is used to redirect input and output file descriptors. Explain why it is necessary to close unused pipe ends after calling dup2(). What could go wrong if you leave pipes open?
 
-_answer here_
+When you use dup2(), it copies one file descriptor onto another. After that, you have extra file descriptors that aren’t needed anymore and closing them is ideal because it frees up resources and prevents any potential issues with file descriptors running out/leaking.
 
 3. Your shell recognizes built-in commands (cd, exit, dragon). Unlike external commands, built-in commands do not require execvp(). Why is cd implemented as a built-in rather than an external command? What challenges would arise if cd were implemented as an external process?
 
-_answer here_
+The cd command is built in instead of being an external command because it needs to change the current working directory of the shell process. If cd was an external command, it would have to run in a separate child process which means that any attempts to change directories would only affect that child process that it is running in. This does not update to the parent and its working directory would be unchanged which makes it impossible for users to actually change directories.
 
 4. Currently, your shell supports a fixed number of piped commands (CMD_MAX). How would you modify your implementation to allow an arbitrary number of piped commands while still handling memory allocation efficiently? What trade-offs would you need to consider?
 
-_answer here_
+To implement an arbitrary number of piped commands while still handling memory allocation efficiently, one could replace the array with a fixed size with a dynamic array or linked list instead. I could implement an  array that grows as needed and resizes while also allocating memory as needed. You could also try to implement a linked list. The trade-offs to consider are that while it would be useful, it would also  make the code harder/complex which could lead to a lot of potential issues especially with resizing and managing memory. 
